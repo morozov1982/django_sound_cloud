3eea87acc271a117303188dd6cba11bc0b2067e4
Alexandr Morozov <morozov1982@gmail.com>
2024-01-10 09:26:30 +0600

    Lesson_05

--- src/oauth/endpoint/views.py
+++ src/oauth/endpoint/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, parsers, permissions
 
-from src.oauth import serializers
+from src.base.permissions import IsAuthor
+from src.oauth import serializers, models
 
 
 class UserView(viewsets.ModelViewSet):
@@ -15,3 +16,21 @@ class UserView(viewsets.ModelViewSet):
 
     def get_object(self):
         return self.get_queryset()
+
+
+class AuthorView(viewsets.ReadOnlyModelViewSet):
+    """Список авторов"""
+    queryset = models.AuthUser.objects.all().prefetch_related('social_links')
+    serializer_class = serializers.AuthorSerializer
+
+
+class SocialLinkView(viewsets.ModelViewSet):
+    """CRUD ссылок соц. сетей пользователей"""
+    serializer_class = serializers.SocialLinkSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return self.request.user.social_links.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- src/oauth/serializers.py
+++ src/oauth/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from src.oauth.models import AuthUser
+from src.oauth.models import AuthUser, SocialLink
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,6 +8,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthUser
         fields = ('avatar', 'country', 'city', 'bio', 'display_name')
+
+
+class SocialLinkSerializer(serializers.ModelSerializer):
+    """Сериализация ссылок соц. сетей"""
+    # id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = SocialLink
+        fields = ('id', 'link')
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    """Сериализация данных автора"""
+    social_links = SocialLinkSerializer(many=True)
+
+    class Meta:
+        model = AuthUser
+        fields = ('id', 'avatar', 'country', 'city', 'bio', 'display_name', 'social_links')
 
 
 class GoogleAuthSerializer(serializers.Serializer):
