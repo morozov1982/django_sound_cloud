d45d959b630e9591bb0b3ee2c4f2a91d27ca70a7
Alexandr Morozov <morozov1982@gmail.com>
2024-01-17 10:26:12 +0600

    Lesson_07

--- src/audio_library/views.py
+++ src/audio_library/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics, viewsets
+import os
+
+from django.http import FileResponse, Http404
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets, parsers, views
 
 from src.audio_library import models, serializers
+from src.base.classes import MixedSerializer, Pagination
 from src.base.permissions import IsAuthor
+from src.base.services import delete_old_file
 
 
 class GenreListView(generics.ListAPIView):
@@ -20,3 +26,120 @@ class LicenseView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class AlbumView(viewsets.ModelViewSet):
+    """CRUD альбомов автора"""
+    parser_classes = (parsers.MultiPartParser,)
+    serializer_class = serializers.AlbumSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return models.Album.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        delete_old_file(instance.cover.path)
+        instance.delete()
+
+
+# Можно было использовать ViewSet, но для примера ListAPIView
+class PublicAlbumView(generics.ListAPIView):
+    """Список публичных альбомов автора"""
+    serializer_class = serializers.AlbumSerializer
+
+    def get_queryset(self):
+        return models.Album.objects.filter(user__id=self.kwargs.get('pk'), private=False)
+
+
+class TrackView(MixedSerializer, viewsets.ModelViewSet):
+    """CRUD треков автора"""
+    parser_classes = (parsers.MultiPartParser,)
+    permission_classes = [IsAuthor]
+    serializer_class = serializers.CreateAuthorTrackSerializer
+    serializer_classes_by_action = {
+        'list': serializers.AuthorTrackSerializer,
+    }
+
+    def get_queryset(self):
+        return models.Track.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        delete_old_file(instance.cover.path)
+        instance.delete()
+
+
+class PlayListView(MixedSerializer, viewsets.ModelViewSet):
+    """CRUD плейлистов пользователя"""
+    parser_classes = (parsers.MultiPartParser,)
+    permission_classes = [IsAuthor]
+    serializer_class = serializers.CreatePlaylistSerializer
+    serializer_classes_by_action = {
+        'list': serializers.PlayListSerializer,
+    }
+
+    def get_queryset(self):
+        return models.Playlist.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        delete_old_file(instance.cover.path)
+        instance.delete()
+
+
+class TrackListView(generics.ListAPIView):
+    """Список всех треков"""
+    queryset = models.Track.objects.all()
+    serializer_class = serializers.AuthorTrackSerializer
+    pagination_class = Pagination
+
+
+class AuthorTrackListView(generics.ListAPIView):
+    """Список всех треков автора"""
+    serializer_class = serializers.AuthorTrackSerializer
+    pagination_class = Pagination
+
+    def get_queryset(self):
+        return models.Track.objects.filter(user__id=self.kwargs.get('pk'))
+
+
+class StreamingFileView(views.APIView):
+
+    def set_play(self, track):
+        track.plays_count += 1
+        track.save()
+
+    def get(self, request, pk):
+        track = get_object_or_404(models.Track, id=pk)
+        if os.path.exists(track.file.path):
+            self.set_play(track)
+            return FileResponse(open(track.file.path, 'rb'), filename=track.file.name)
+        else:
+            return Http404
+
+
+class DownloadTrackView(views.APIView):
+    """Скачивание трека"""
+
+    def set_download(self):
+        self.track.downloads += 1
+        self.track.save()
+
+    def get(self, request, pk):
+        self.track = get_object_or_404(models.Track, id=pk)
+        if os.path.exists(self.track.file.path):
+            self.set_download()
+            return FileResponse(
+                open(self.track.file.path, 'rb'),
+                filename=self.track.file.name,
+                as_attachment=True,
+            )
+        else:
+            return Http404
--- src/base/services.py
+++ src/base/services.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.core.exceptions import ValidationError
 
 
@@ -8,17 +10,17 @@ def get_path_upload_avatar(instance, filename):
 
 def get_path_upload_cover_album(instance, filename):
     """Построение пути к файлу обложки альбома, format: (media)/album/<user_id>/<filename>"""
-    return f"album/user_{instance.id}/{filename}"
+    return f"album/user_{instance.user.id}/{filename}"
 
 
 def get_path_upload_track(instance, filename):
     """Построение пути к треку, format: (media)/track/<user_id>/<filename>"""
-    return f"track/user_{instance.id}/{filename}"
+    return f"track/user_{instance.user.id}/{filename}"
 
 
 def get_path_upload_cover_playlist(instance, filename):
     """Построение пути к обложке плейлиста, format: (media)/playlist/<user_id>/<filename>"""
-    return f"playlist/user_{instance.id}/{filename}"
+    return f"playlist/user_{instance.user.id}/{filename}"
 
 
 def validate_size_image(image):
@@ -26,3 +28,9 @@ def validate_size_image(image):
     megabytes_limit = 2
     if image.size > 1024 * 1024 * megabytes_limit:
         raise ValidationError(f"Максимальный размер файла {megabytes_limit} Мб")
+
+
+def delete_old_file(path_file):
+    """Удаление старого файла"""
+    if os.path.exists(path_file):
+        os.remove(path_file)
